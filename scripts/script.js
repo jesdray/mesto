1db14b6fd79264f1d04600f6deb89a5710ae55b7
Evgeny Shumilov <ttrrooll700@yandex.ru>
2020-12-06 20:27:50 +0700

    feat: добавить удаление и закрузку картинок

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,99 @@
 let info = document.querySelector('.profile__info');
 let openedPopup = document.querySelector('.profile__edit-button');
-let input = document.querySelector('.popup');
-let closedPopup = document.querySelector('.popup__close');
-let savePopup = document.querySelector('.popup__button')
-let form = document.querySelector('.popup__container');
+let openedAddnewimagePopup = document.querySelector('.profile__add-button')
+const input = document.querySelectorAll('.popup');
+const closedPopup = document.querySelectorAll('.popup__close');
+const savePopup = document.querySelectorAll('.popup__button')
+const form = document.querySelectorAll('.popup__container');
 
 let name = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__job');
 let inputName = document.querySelector('.popup__text_script_name');
 let inputJob = document.querySelector('.popup__text_script_job');
 
-function openPopup() {
-    input.classList.add('popup_opened');
+const elements = document.querySelector('.elements');
+const cardsTemplate = document.querySelector('.element-template')
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function renderList() {
+    const listItem = initialCards.map(addImage);
+    elements.append(...listItem);
+};
+
+function addImage(item) {
+    const cardsElement = cardsTemplate.content.cloneNode(true);
+    const elementImage = cardsElement.querySelector('.element__image');
+    const elementName = cardsElement.querySelector('.element__image-name');
+    cardsElement.querySelector('.element__button-like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('element__button-like_active')
+    });
+    cardsElement.querySelector('.element__trash').addEventListener('click', removeImage );
+    elementImage.src = item.link;
+    elementImage.alt = item.name;
+    elementName.textContent = item.name;
+    return cardsElement;
+};
+
+function addNewImage() {
+
+}
+
+function removeImage(evt) {
+    const targetElement = evt.target;
+    const targetItem = targetElement.closest('.element');
+    targetItem.remove();
+}
+
+function openClosePopup() {
+    input.classList.toggle('popup_opened');
     inputName.value = name.textContent;
     inputJob.value = job.textContent;
 }
 
-function closePopup() {
-    input.classList.remove('popup_opened');
-}
-
-function formSubmitHandler(event) {
-    event.preventDefault();
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     name.textContent = inputName.value;
     job.textContent = inputJob.value;
-    closePopup()
+    openClosePopup()
 }
+
+console.log(input);
+console.log(closedPopup);
+console.log(savePopup);
+console.log(form);
+
+renderList();
 
 input.addEventListener('submit', formSubmitHandler);
 
-openedPopup.addEventListener('click', openPopup);
+openedPopup.addEventListener('click', openClosePopup);
 
-closedPopup.addEventListener('click', closePopup);
+openedAddnewimagePopup.addEventListener('click', openClosePopup)
+
+closedPopup.addEventListener('click', openClosePopup);
